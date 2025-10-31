62d30ecdbea15543400593a2697f5e5601631479
4CeL <137417951+4CeL@users.noreply.github.com>
2025-10-31 08:28:27 +0700

    Delete src directory

--- src/main/java/com/example/demo/repository/StatusMasterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.StatusMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatusMasterRepository extends JpaRepository<StatusMaster, Integer> {
-}
